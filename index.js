f9824b62ce654c76a327ab7f6f71709efaadf657
shlomihillel <hi.shlomi@gmail.com>
2021-07-15 01:13:05 +0300

    index

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser'); //why?
+
 
 const app = express();
-const roomsRouter = require('./routes/rooms.route');
+const userRouter = require('./routes/users.route'); //middileware?
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false})); //middileware?
+app.use(bodyParser.json()); //middileware?
 
-app.use('/api/rooms', roomsRouter)
+app.use('/api/users', userRouter); //middileware?
 
-//connect to db with mongoose
-mongoose.connect('mongodb://localhost/somecollection', {
+const port = process.env.PORT || 5000;
+
+const uri =  'mongodb+srv://test:test@cluster0.txwkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
@@ -20,6 +24,15 @@ mongoose.connect('mongodb://localhost/somecollection', {
     console.log("database connect")
 });
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`application start at ${process.env.PORT || 5000}`)
-})
+
+
+app.listen(port, () => {
+    console.log('app is running on port:' + port)
+});
+
+
+// app.listen(process.env.PORT || 5000, () => {
+//     console.log(`application start at ${process.env.PORT || 5000}`)
+// })
+
+
